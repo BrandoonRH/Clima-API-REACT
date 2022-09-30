13aa6a8f16a7adad3ff4e2bbb9d9a106d72455f1
BrandoonRH <aeroatlas.51@gmail.com>
2022-09-30 14:51:43 -0500

    Cambios en la URL http's'

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -23,7 +23,7 @@ const concultarClima = async(datas) => {
     try {
         const {city, country } = datas; 
         const appId = import.meta.env.VITE_API_KEY; 
-        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=1&appid=${appId}`; 
+        const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=1&appid=${appId}`; 
         const {data} = await axios(url); 
         const {lat, lon } = data[0]
         const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
